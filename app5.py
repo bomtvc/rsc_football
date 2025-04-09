53808b6fe728196f72cd03cb51fc017b703195a3
bomtvc <32728677+bomtvc@users.noreply.github.com>
2025-04-09 15:42:57 +0700

    Update app5.py

--- app5.py
+++ app5.py
@@ -12,7 +12,7 @@ import base64
 st.set_page_config(page_title="Bốc Thăm Bảng Đấu", layout="wide")
 
 # Tiêu đề ứng dụng
-st.title("🏆 BỐC THĂM BẢNG THI ĐẤU GIẢI BÓNG ĐÁ TRUYỀN THỐNG ROCHDALE SPEARS 🏆")
+st.title("🏆 BỐC THĂM BẢNG THI ĐẤU GIẢI BÓNG ĐÁ TRUYỀN THỐNG ROCHDALE SPEARS 🏆\n Official Group Stage Draw – Rochdale Spears Traditional Football Tournament 2025")
 
 # Khởi tạo session state
 if 'positions' not in st.session_state:
